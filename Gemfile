980db4869ac5249093f93ea912fa86d9f31485b6
Josué Gama <josue.figueroa@magmalabs.io>
2020-01-14 11:10:42 -0600

    Finished user signup

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Pry
   gem 'pry-rails'
+  # Include controller testing
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
@@ -20,8 +20,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-    :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
